1794f25825b02c535bec568d7071d0e8735efea5
unknown <htcsurajtalole@gmail.com>
2024-07-01 22:18:21 +0530

    todays change

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from "react";
 import Banner from "../components/Banner";
+import Card from "../components/Card";
+import Jobs from "./Jobs";
+import Sidebar from "../sidebar/Sidebar";
 
 const Home = () => {
   const [selectedCategory, setSelectedCategory] = useState(null);
@@ -12,24 +15,80 @@ const Home = () => {
         setJobs(data);
       })
       .catch((err) => console.error(err));
-    // console.log(jobs);
   }, []);
 
-  //handle input change
+  // Handle input change
   const [query, setQuery] = useState("");
   const handleInputChange = (event) => {
     setQuery(event.target.value);
   };
 
-  //filter jobs by tile
+  // Filter jobs by title
   const filteredItem = jobs.filter(
     (job) => job.jobTitle.toLowerCase().indexOf(query.toLowerCase()) !== -1
   );
-  console.log(filteredItem);
+
+  // Radio filtering
+  const handleChange = (event) => {
+    setSelectedCategory(event.target.value);
+  };
+
+  // Button based filtering
+  const handleClick = (event) => {
+    setSelectedCategory(event.target.value);
+  };
+
+  // Main Function
+  const filteredData = (jobs, selected, query) => {
+    let filteredJobs = jobs;
+
+    // Filtering input items
+    if (query) {
+      filteredJobs = filteredItem;
+    }
+
+    // Category filtering
+    if (selected) {
+      filteredJobs = filteredJobs.filter(
+        ({
+          jobLocation,
+          maxPrice,
+          experienceLevel,
+          salaryType,
+          employmentType,
+          postingDate,
+        }) =>
+          jobLocation.toLowerCase() === selected.toLowerCase() ||
+          parseInt(maxPrice) <= parseInt(selected) ||
+          salaryType.toLowerCase() === selected.toLowerCase() ||
+          employmentType.toLowerCase() === selected.toLowerCase()
+      );
+    }
+
+    return filteredJobs.map((data, i) => <Card key={i} data={data} />);
+  };
+
+  const result = filteredData(jobs, selectedCategory, query);
 
   return (
     <div>
       <Banner query={query} handleInputChange={handleInputChange} />
+
+      {/* Main Content */}
+      <div className="bg-[#FAFAFA] md:grid grid-cols-4 gap-8 lg:px-24 px-4 py-12">
+        {/* left side */}
+        <div className="bg-white p-4 rounded">
+          <Sidebar handleChange={handleChange} handleClick={handleClick} />
+        </div>
+
+        {/* job card */}
+        <div className="col-span-2 bg-white p-4 rounded-sm">
+          <Jobs result={result} />
+        </div>
+
+        {/* right side */}
+        <div className="bg-white p-4 rounded">Right</div>
+      </div>
     </div>
   );
 };
