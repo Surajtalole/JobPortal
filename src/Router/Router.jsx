e3448dc63ec6a0b2369dbaca08f598751455635e
unknown <htcsurajtalole@gmail.com>
2024-07-21 19:34:19 +0530

    changes

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -3,6 +3,7 @@ import App from "../App";
 import Home from "../Pages/Home";
 import About from "../Pages/About";
 import CreateJob from "../Pages/CreateJob";
+import MyJobs from "../Pages/MyJobs";
 const router = createBrowserRouter([
     {
       path: "/",
@@ -12,6 +13,10 @@ const router = createBrowserRouter([
         {
           path:"/post-job",
           element:<CreateJob/>
+        },
+        {
+          path:"/my-job",
+          element:<MyJobs/>
         }
         
    
